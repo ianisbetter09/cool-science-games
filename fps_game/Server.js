fe9cedcf5a9f3bca75e6cdf8c3e8e2290e39453f
Minecraft003003 <ian.gintner0722@gmail.com>
2025-04-19 00:16:16 -0700

    Update Server.js

--- fps_game/Server.js
+++ fps_game/Server.js
@@ -7,29 +7,34 @@ app.use(express.static('public'));
 
 const players = {};
 
-io.on('connection', (socket) => {
-  console.log(`New player: ${socket.id}`);
+io.on('connection', socket => {
+  console.log('New player:', socket.id);
 
-  players[socket.id] = { x: 100, y: 100, id: socket.id };
-
+  players[socket.id] = { id: socket.id, x: 0, y: 5, z: 0 };
   socket.emit('currentPlayers', players);
   socket.broadcast.emit('newPlayer', players[socket.id]);
 
-  socket.on('playerMovement', (data) => {
+  socket.on('playerMovement', data => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
       players[socket.id].y = data.y;
-      socket.broadcast.emit('playerMoved', players[socket.id]);
+      players[socket.id].z = data.z;
+      socket.broadcast.emit('playerMoved', {
+        id: socket.id,
+        x: data.x,
+        y: data.y,
+        z: data.z
+      });
     }
   });
 
   socket.on('disconnect', () => {
+    console.log('Player disconnected:', socket.id);
     delete players[socket.id];
     io.emit('playerDisconnected', socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+http.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
